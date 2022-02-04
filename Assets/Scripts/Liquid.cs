8323c494d0813eccf149ac97210e3f4b9f858553
w2leo <82013108+w2leo@users.noreply.github.com>
2022-02-04 20:36:03 +0300

    Add class Liquid

---
+++ Assets/Scripts/Liquid.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Liquid
+{
+    /// <summary>
+    /// Liquid Class for mathematical description 
+    /// flowSpeed - now const, but later can be variable
+    /// </summary>
+    private double D;
+    private const double flowSpeed = 1.0f;
+
+    private double CountPipeSectionArea(double pipeDiameter)
+    {
+        return Math.PI * Math.Pow(pipeDiameter, 2) / 4;
+    }
+
+    /// <summary>
+    /// Count Liquid Consumption m3 / s. Requires D - pipe diameter
+    /// </summary>
+    /// <param name="pipeSectionArea"></param>
+    /// <returns></returns>
+    public double CountLiquidConsumption(double pipeDiameter)
+    {
+        return CountPipeSectionArea(pipeDiameter) * flowSpeed;
+    }
+}
