d7f0bcfb0591e5aebfe593d88a4584380dce99e3
w2leo <82013108+w2leo@users.noreply.github.com>
2022-02-06 14:58:49 +0300

    Rotation in work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotateSpeed;
+    private Vector3 mousePosition;
+
+    private bool isRotating;
+
     private Vector3 movement
     {
         get
@@ -15,22 +20,54 @@ public class PlayerController : MonoBehaviour
             return new Vector3(horizontal, 0f, vertical);
         }
     }
+    private Vector3 rotation
+    {
+        get
+        {
+            Vector3 currentPosition = Input.mousePosition;
+            Vector3 deltaPosition = currentPosition - mousePosition;
+            mousePosition = currentPosition;
+            return deltaPosition;
+        }
+    }
+
+    private void Start()
+    {
+        isRotating = false;
+    }
 
     private void Update()
     {
         MovePlayer();
+
+        if (!isRotating && Input.GetMouseButtonDown(1))
+        {
+            isRotating = true;
+            mousePosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            isRotating = false;
+        }
+
+        if (isRotating)
+            RotatePlayer();
     }
 
     private void MovePlayer()
     {
-         transform.Translate(movement, transform);
+        transform.Translate(movement * moveSpeed * Time.deltaTime, transform);
     }
 
-    private void RotatePlayer(float direction)
+    private void RotatePlayer()
     {
         //transform.RotateAround(Vector3.up, direction * rotateSpeed);
-        transform.Rotate(Vector3.up, direction * rotateSpeed);
+        //Debug.Log(rotation);
+        var x = Quaternion.Euler(rotation).eulerAngles;
+        Debug.Log($"x = {x}");
+        //transform.Rotate(Quaternion.Euler(rotation), rotateSpeed * Time.deltaTime);
+       // transform.Rotate(x);
+        transform.localRotation = Quaternion.Euler(rotation);
     }
-
-
 }
